052825a404468723318aa4efefaf3d9834a67bac
Aynh <99479536+Aynh@users.noreply.github.com>
2022-12-03 01:05:20 +0800

    feat: strip ansi when stdout cannot show color

--- kbbi.v
+++ kbbi.v
@@ -1,8 +1,9 @@
 module main
 
-import os
 import client { KbbiClient, new_client_from_login }
 import format { format_result }
+import os
+import term
 
 fn create_client() ?KbbiClient {
 	return new_client_from_login(
@@ -15,7 +16,12 @@ fn main() {
 	c := create_client() or { KbbiClient{''} }
 
 	if results := c.entry(os.args[1]) {
-		print(results.map(format_result).join('\n'))
+		mut out := results.map(format_result).join('\n')
+		if !term.can_show_color_on_stdout() {
+			out = term.strip_ansi(out)
+		}
+
+		print(out)
 	} else {
 		println(err)
 		exit(1)
