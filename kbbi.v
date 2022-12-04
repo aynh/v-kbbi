57eb384c0805df625049707d4a2f371095a6ac9c
Aynh <99479536+Aynh@users.noreply.github.com>
2022-12-04 14:33:09 +0800

    feat: support multiple argument

--- kbbi.v
+++ kbbi.v
@@ -18,24 +18,29 @@ fn main() {
 	vm := vmod.decode(@VMOD_FILE) or { panic(err) }
 	mut app := cli.Command{
 		name: vm.name
-		usage: '<word>'
+		usage: '<word>...'
 		description: vm.description
 		version: vm.version
 		required_args: 1
 		execute: fn (cmd cli.Command) ! {
 			c := create_client() or { client.new_client()! }
 
-			if results := c.entry(cmd.args[0]) {
-				mut out := results.map(format_result).join('\n')
-				if !term.can_show_color_on_stdout() {
-					out = term.strip_ansi(out)
+			mut results := []string{cap: cmd.args.len}
+			for word in cmd.args {
+				w_results := c.entry(word) or {
+					println('failed to search `${word}`: ${err}')
+					exit(1)
 				}
 
-				print(out)
-			} else {
-				println(err)
-				exit(1)
+				results << w_results.map(format_result).join('\n')
 			}
+
+			mut out := results.join('\n')
+			if !term.can_show_color_on_stdout() {
+				out = term.strip_ansi(out)
+			}
+
+			print(out)
 		}
 	}
 
