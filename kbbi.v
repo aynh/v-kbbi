1f2cb11979c0e0aa9809ee3b323539761c81d528
Aynh <99479536+Aynh@users.noreply.github.com>
2022-12-04 14:15:54 +0800

    feat: add simple CLI parsing

--- kbbi.v
+++ kbbi.v
@@ -1,9 +1,11 @@
 module main
 
+import cli
 import client
 import format { format_result }
 import os
 import term
+import v.vmod
 
 fn create_client() ?client.KbbiClient {
 	return client.new_client_from_login(
@@ -13,17 +15,30 @@ fn create_client() ?client.KbbiClient {
 }
 
 fn main() {
-	c := create_client() or { client.new_client()! }
+	vm := vmod.decode(@VMOD_FILE) or { panic(err) }
+	mut app := cli.Command{
+		name: vm.name
+		usage: '<word>'
+		description: vm.description
+		version: vm.version
+		required_args: 1
+		execute: fn (cmd cli.Command) ! {
+			c := create_client() or { client.new_client()! }
 
-	if results := c.entry(os.args[1]) {
-		mut out := results.map(format_result).join('\n')
-		if !term.can_show_color_on_stdout() {
-			out = term.strip_ansi(out)
+			if results := c.entry(cmd.args[0]) {
+				mut out := results.map(format_result).join('\n')
+				if !term.can_show_color_on_stdout() {
+					out = term.strip_ansi(out)
+				}
+
+				print(out)
+			} else {
+				println(err)
+				exit(1)
+			}
 		}
-
-		print(out)
-	} else {
-		println(err)
-		exit(1)
 	}
+
+	app.setup()
+	app.parse(os.args)
 }
