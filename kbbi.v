b5073e4d0e69208f40b275b187b674765b37f57d
Aynh <99479536+Aynh@users.noreply.github.com>
2022-12-05 20:12:18 +0800

    feat: add --json cli switch to output JSON

--- kbbi.v
+++ kbbi.v
@@ -3,6 +3,7 @@ module main
 import cli
 import client
 import format { format_result }
+import json
 import os
 import spinner
 import term
@@ -38,6 +39,11 @@ fn main() {
 				description: 'Ignores cached response.'
 				global: true
 			},
+			cli.Flag {
+				flag: cli.FlagType.bool
+				name: 'json'
+				description: 'Outputs in JSON format.'
+			}
 		]
 		required_args: 1
 		execute: fn (cmd cli.Command) ! {
@@ -47,19 +53,25 @@ fn main() {
 			no_cache := cmd.flags.get_bool('no-cache')!
 			c := create_client(use_cache: !no_cache) or { client.new_client(use_cache: !no_cache)! }
 
-			mut results := []string{cap: cmd.args.len}
+			mut results := []client.KbbiResult{}
 			for word in cmd.args {
 				w_results := c.entry(word) or {
 					println('failed to search `${word}`: ${err}')
 					exit(1)
 				}
 
-				results << w_results.map(format_result).join('\n')
+				results << w_results
 			}
 
-			mut out := results.join('\n')
-			if !(cmd.flags.get_bool('color')! && term.can_show_color_on_stdout()) {
-				out = term.strip_ansi(out)
+			out := if cmd.flags.get_bool('json')! {
+				json.encode(results)
+			} else {
+				mut tmp := results.map(format_result).join('\n')
+				if cmd.flags.get_bool('color')! && term.can_show_color_on_stdout() {
+				 	tmp
+				} else {
+					term.strip_ansi(tmp)
+				}
 			}
 
 			lock spinner_state {
