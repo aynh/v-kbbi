ab475fe2328d5e96497f3c80979416eab5317c24
Aynh <99479536+Aynh@users.noreply.github.com>
2022-12-05 18:32:57 +0800

    fix: don't add space if we're gonna add a newline

--- format/wrap_word.v
+++ format/wrap_word.v
@@ -17,15 +17,13 @@ fn wrap_word(c WrapWordConfig) string {
 	builder.write_string(c.prefix)
 
 	mut char_count := 0
-	max_width := c.max_width - c.prefix.len - 1
-	for i, word in c.s.split(' ') {
-		if char_count != 0 && i > 0 {
+	max_width := c.max_width - c.prefix.len
+	for word in c.s.split(' ') {
+		word_without_ansi := term.strip_ansi(word)
+		if (char_count + word_without_ansi.len) < max_width {
 			builder.write_string(' ')
 			char_count += 1
-		}
-
-		word_without_ansi := term.strip_ansi(word)
-		if (char_count + word_without_ansi.len) > max_width {
+		} else {
 			builder.write_string('\n' + c.prefix)
 
 			char_count = 0
