8d77c4ce0e3b39137002f3cad1a9eedd9141fa02
aynh <aynh@users.noreply.github.com>
2023-01-01 06:01:17 +0000

    deploy: d8c26797447c096f0e23c702120dc9ba52925238

---
+++ search_index.js
@@ -0,0 +1,4 @@
+var searchModuleIndex = ["kbbi",];
+var searchIndex = ["Client","entry","is_logged_in","ClientFromLoginConfig","new_client_from_login","Entry","EntryItem","EntryExampleItem","EntryKindItem","entry",];
+var searchModuleData = [["","kbbi.html"],];
+var searchData = [["kbbi","","kbbi.html#Client","struct "],["kbbi","","kbbi.html#Client.entry","fn (Client)"],["kbbi","","kbbi.html#Client.is_logged_in","fn (Client)"],["kbbi","","kbbi.html#ClientFromLoginConfig","struct "],["kbbi","","kbbi.html#new_client_from_login","fn "],["kbbi","","kbbi.html#Entry","struct "],["kbbi","","kbbi.html#EntryItem","struct "],["kbbi","","kbbi.html#EntryExampleItem","struct "],["kbbi","","kbbi.html#EntryKindItem","struct "],["kbbi","","kbbi.html#entry","fn "],];
